0d523f06cc313d380958c689a75e8cdcc9ff8b11
Sören Tempel <soeren+git@soeren-tempel.net>
2018-01-05 11:44:11 +0100

    util.h: add zsnprintf and use it in the xsnprintf macro
    zsnprintf returns 0 if an error occurred and can therefore return an
    unsigned integer data type. This is useful because it allows comparison with
    an unsiged type such as size_t and besides we now properly handle
    snprintf errors by returning zero from our status function thereby
    indicating that no bytes were written to the status string.

--- util.h
+++ util.h
@@ -4,6 +4,10 @@
 		exit(EXIT_FAILURE);                                            \
 	} while (0)
 
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
+
+#define zsnprintf(DEST, SIZE, FORMAT, ...) \
+	(unsigned int)MAX(0, snprintf(DEST, SIZE, FORMAT, __VA_ARGS__))
 #define xsnprintf(DEST, SIZE, FORMAT, ...) \
-	MIN(SIZE, snprintf(DEST, SIZE, FORMAT, __VA_ARGS__))
+	MIN(SIZE, zsnprintf(DEST, SIZE, FORMAT, __VA_ARGS__))
